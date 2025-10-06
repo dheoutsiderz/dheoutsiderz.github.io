c26afc2a42e636dd30a6fe6b40165d5cbee36cf2
DheOutSiderz <93863680+dheoutsiderz@users.noreply.github.com>
2025-10-06 15:16:43 +0700

    Create emote.lua

---
+++ roblox/emote.lua
@@ -0,0 +1,592 @@
+-- GUI contents
+AnimButtons = {
+	98719422024341, "SEIZURE",
+	79075971527754, "Flopping Fish",
+	131063357988754, "Levitate",
+	11394033602, "Elton John - Elevate",
+	134283166482394, "Skibidi Toilet Titan Speakerman Laser Spin",
+	110792133024438, "Spin1",
+	91004858616595, "Spin2",
+	100935687246938, "Spinning",
+	80416220413470, "Microwave Spin",
+	113821268084684, "Head Spin1",
+	124522818196151, "Head Spin2",
+	126008456731780, "Spin Me",
+	135373056067761, "Tornado1",
+	110990913678013, "Tornado2",
+	105128045372416, "Monkey Spin",
+	105128045372416, "Homer Floor Spin",
+	95329943746305, "Hovering Spin",
+	76950177647300, "Funky Touhou Spin",
+	96827306464973, "Hover Torso Spin",
+	130065247828069, "SLOW 360 SPIN",
+	73178676975054, "Spinning Dancing Roach",
+	115204251208617, "Plushie Sit Spin",
+	80879471830819, "Spin Around with a Friend Matching",
+	133779321964805, "Particle Accelerator Spin",
+	85058233989583, "Goofy Spin",
+
+	-- ANOMALY
+	78413960254297, "Stylish Head Spin CHEAP",
+	127212897044971, "invisible-me",
+	134386657144069, "e invisible BEST",
+	76510079095692, "Helicopter",
+	104493858272363, "Floating Bodyparts",
+	124729247977888, "Around the Head",
+	86036950557685, "Glitch through walls",
+	107728954756412, "RAPID FIRE Gun Morph",
+	96731289267640, "Speed Mirage",
+	115569573258316, "Little Obbyist",
+	80120791952773, "Speed Glitch+",
+	115176965319158, "New Best No clip and Speed",
+	
+
+    -- POSES
+	10713992055, "Bored",
+	83026903211659, "Police Vest Rest Pose",
+	79137761903782, "Gangster Pose",
+	113694692828125, "Wall Lean Pose",
+	10714347256, "Godlike",
+	117181705578424,"Cute Hips Profile Pose",
+	87613777175338, "Wall Lean (Dynamic Face Animation V1)",
+	112018126760165, "Wall Lean",	
+	78848614632586, "Chill Pose - wall",
+	
+	-- FLOATING
+	93302851761572, "Super Cool Float",
+	109030594660124, "Gojo Floating The Honored One",
+	102342919277367, "Floating in Love",
+	96694088279578, "Floating In Space",
+	137847348761702, "Floating Spin",
+	135311171741053, "HOT Spinning Cat",
+	77840765435893, "Floating on clouds",
+	70375615605401, "Aura Pose Floating KING",
+	123955307291627, "SpiderMan Hanging Pose",
+	83345430870757, "Spiderman Hang",
+	133418516499878, "Effortless Aura Pose",
+	132625096369457, "Floating Immortality Cultivator Meditation",	
+	108540721793164, "Floating Relaxation",
+	133523945826598, "Spooky Float",
+	88225720130440, "Unbothered Aura Floating Sitting",
+	84179650680787, "Floating Sitting",
+	103853175871097, "Zen floating meditation emote",
+	-- HUG
+	88875926125122, "cute plush hug",
+	132014993199931, "Cute Hug Pose 1 MATCHING",
+	73124906770925, "Hug Emote",
+	118264035209903, "Sweet Hug Pose V2 MATCHING",
+	118478639008343, "HUG",
+	88383446061342, "Cozy Hug Pose V1 [MATCHING]",
+	115731801744043, "Cute hug + petting",
+	100032088866413, "MATCHING Sitting Hug 1",
+	105341373580564, "Cute Matching Sit Hug 2-2",
+	85638447484999, "cute plushie hug sit",
+	
+	
+	
+	-- RUN & WALK
+	81981358389159, "Fake R15 Zombie Run",
+	
+	-- FLY
+	91133306449431, "e-fly",
+	89523370947906, "FLY2",
+	
+	-- SIT
+	127626736897320, "Demon Slayer Douma Sit",
+	126899447275562, "Nonchalant Sit",
+	102544119718369, "Sit1",
+	77139456213922, "Aura Sit",
+	
+	
+	-- SLeep
+	82858845963128, "Sleep",
+	115306317108861, "Sleeping",
+	118422508938639, "Fake Death",
+	84112287597268, "MM2 Fake Dead",
+	84114319413819, "Playing Dead",
+	100118897356565, "Sleeping Sideways",
+	104270330131897, "Crying Pose",	
+	123822744060080, "Lying On Stomach Pose",
+	103228987744330, "Laying Cutely",
+	88055075246852, "Cute Kicking Feet Lay",
+	98426774209184, "cute laying legs up idle",
+	120213959413096, "chill laying",
+	
+	
+	
+    -- Ngece
+	96435804447949, "WOOF BARK WOOF",
+	139310328821985, "silly-jumping-spider-dance",
+	99195149999804, "Tuff",
+	93648818551396, "Head Bang",
+	127415121971326, "MUAHAHAHA-EVIL-VILLAIN-LAUGH",
+	138706623195970, "Arthur Morgan Coughing",
+	80933111363555, "Boxing1",
+	87683757107994, "Boxing2",
+	108917274123277,"The shmoves",
+	
+	-- CUTE	
+	124287251935400, "Cute-Feet-Kicking",
+	
+	
+	
+	-- DANCE
+	139110277540140, "BEST-Soda-Pop",
+	78684440273676, "Rat Dance",
+	126786971779567, "Jason-Vorhees-Emote",
+	87282442358351, "Jason-Vorhees-Emote-ORIGINAL",
+	138694026614242, "Michael-Myers-Shake-Dance-Emote",
+	138647889956297, "Jumpstyle Dance",
+	96147994216119, "MOSH",
+	111272262660539, "IShowSpeed-Bounce-Dance",
+	99558490932154, "Sturdy Dance - Ice Spice [CHEAP]",
+	132967433846099, "Sturdy Dance",
+	130445733737505, "Die Lit",
+	126780665379004, "Trip Out",
+	108025254624123, "Nyan Nyan!",
+	73517007972710, "Baddie Hips",
+	125578981255289, "Take The L",
+	83650099589962, "Koto Nai Meme Dance",
+	85247727536511, "Jubi Slide",
+	122719596509695, "Rock Out",
+	104748118296461, "TV Time Dance",
+	118468821959324, "Shuffling",
+	106984923725161, "Beat Drop Shuffle",
+
+
+	}
+
+
+
+
+-- General Varibles
+CursedMode = false
+Player = game.Players.LocalPlayer
+Mouse = Player:GetMouse()
+HeaderSize = 25
+PlayingId = 0
+Red = Color3.new(0.9, 0, 0)
+Green = Color3.new(0.26, 0.85, 0)
+Grey = Color3.new(0.6, 0.6, 0.6)
+Blue = Color3.new(0.26, 0.53, 0.95)
+DarkGrey = Color3.fromRGB(30,30,30)
+Maroon = Color3.fromRGB(102,0,0)
+
+-- Setting up the GUI
+ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "MyAnimationHub"
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ResetOnSpawn = false
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+function CreateFrame(Name)
+	local Frame = Instance.new("ScrollingFrame")
+	Frame.Visible = false
+	Frame.Size = UDim2.new(0,213,0,300)
+	Frame.BackgroundColor3 = Color3.new(0.286168, 0.286168, 0.286168)
+	Frame.CanvasSize = UDim2.new(0,0,0,0)
+	Frame.Name = Name
+	Frame.Parent = ScreenGui
+	Instance.new("UICorner", Frame)
+	
+	return Frame
+end
+
+AnimationsFrame = CreateFrame("AnimationsTabFrame")
+AnimationsFrame.Visible = true
+StolenAnimationsFrame = CreateFrame("StolenAnimationsTabFrame")
+
+TabsFrame = Instance.new("Frame")
+TabsFrame.Size = UDim2.new(0,20,0,300)
+TabsFrame.BackgroundTransparency = 1
+TabsFrame.Position = UDim2.new(0, 213,0, 0)
+TabsFrame.Parent = ScreenGui
+
+RenameBox = Instance.new("TextBox",ScreenGui)
+RenameBox.Size = UDim2.new(0.3,0,0.15,0)
+RenameBox.BackgroundColor3 = Color3.new(0.286168, 0.286168, 0.286168)
+RenameBox.PlaceholderText = "Enter new name"
+RenameBox.AnchorPoint = Vector2.new(0.5,0.5)
+RenameBox.Position = UDim2.new(0.5,0,0.5,0)
+RenameBox.Text = ""
+RenameBox.TextScaled = true
+RenameBox.RichText = true
+RenameBox.Visible = false
+
+DragBar = Instance.new("TextButton")
+DragBar.Text = ""
+DragBar.AutoButtonColor = false
+DragBar.Position = UDim2.new(0,AnimationsFrame.AbsolutePosition.X-7.5,0, AnimationsFrame.AbsolutePosition.Y)
+DragBar.Size = UDim2.new(0,230,0,25)
+DragBar.BackgroundColor3 = Color3.new(0.133272, 0.133272, 0.133272)
+Instance.new("UICorner").Parent = DragBar
+DragBar.Position -= UDim2.new(0,0,0,25)
+DragBar.Parent = ScreenGui
+
+CloseButton = Instance.new("TextButton")
+CloseButton.Text = "[X]"
+CloseButton.Position = UDim2.new(0,AnimationsFrame.AbsolutePosition.X-7.5,0, AnimationsFrame.AbsolutePosition.Y)
+CloseButton.Size = UDim2.new(0,25,0,25)
+CloseButton.BackgroundColor3 = Red
+Instance.new("UICorner",CloseButton)
+CloseButton.Position -= UDim2.new(0,0,0,25)
+CloseButton.Parent = ScreenGui
+
+local Title = Instance.new("TextLabel")
+Title.Position = UDim2.new(0,0,0,-125)
+Title.Size = UDim2.new(0,213,0,50)
+Title.Name = "Title"
+Title.BorderSizePixel = 0
+Title.BackgroundTransparency = 1
+Title.TextScaled = true
+Title.TextColor = BrickColor.new("Institutional white")
+Title.Text = "EMOTE RUSUH"
+Instance.new("UIStroke").Parent = Title
+Title.Position = UDim2.new(0,AnimationsFrame.AbsolutePosition.X,0, AnimationsFrame.AbsolutePosition.Y - Title.Size.Y.Offset*1.5)
+Title.Parent = ScreenGui
+
+-- Create anim instance
+Anim = Instance.new("Animation", script)
+
+-- General functions
+
+function Layout(Frame)
+	local Layout = Instance.new("UIListLayout", Frame)
+	Layout.FillDirection = Enum.FillDirection.Horizontal
+	Layout.Wraps = true
+	Layout.SortOrder = Enum.SortOrder.LayoutOrder
+end
+
+function NiceButtons(Button)
+	Instance.new("UICorner").Parent = Button
+	local Grad = Instance.new("UIGradient")
+	Grad.Color = ColorSequence.new{
+		ColorSequenceKeypoint.new(0, Color3.new(1,1,1)),
+		ColorSequenceKeypoint.new(1, Color3.new(0.7,0.7,0.7)),
+	}
+	Grad.Parent = Button
+end
+
+function CreateInteractable(Size, Name, Text, Color, X, Button, Class, Tab)
+	local Element = Instance.new(Class,Tab)
+	if Button then
+		Element.Name = Name.. "Button"
+	else
+		Element.Name = Name.. "Box"
+	end
+	if Button then
+		Element.Text = Text
+	else
+		Element.Text = ""
+		Element.PlaceholderText = Text
+	end
+	Element.Size = Size
+	Element.TextScaled = true
+	Element.RichText = true
+	Element.BackgroundColor3 = Color
+	NiceButtons(Element)
+	return Element
+end
+
+function MassUnToggle(Button)
+	for Num, Frame in ScreenGui:GetChildren() do
+		if Frame:IsA("ScrollingFrame") then
+			for Num, Butn in Frame:GetChildren() do
+				if Butn:IsA("TextButton") and Butn ~= Button then
+					Butn:SetAttribute("Toggled", false)
+					Butn.BackgroundColor3 = Red
+				end
+			end
+		end
+	end
+end
+
+function CreateButton(Size,Color,Text, Name,ID, Frame, ConnectingBox, Toggled)
+	local Button = CreateInteractable(Size, Name, Text, Color, 100, true, "TextButton", Frame)
+	Button:SetAttribute("Toggled", Toggled)
+	Button:SetAttribute("AnimId", ID)
+	Frame.CanvasSize += UDim2.new(0,0,0,Size.Y.Offset)
+	Button.MouseButton1Click:Connect(function()
+		--if not (PlayingId == ID or PlayingId == 0) and not CursedMode then return end
+		--print(ID)
+		Button:SetAttribute("Toggled", not Button:GetAttribute("Toggled"))
+		if Track and not CursedMode then
+			Track:Stop()
+		end
+		if Button:GetAttribute("Toggled") then
+			MassUnToggle(Button)
+			PlayingId = ID
+			Button.BackgroundColor3 = Maroon
+			--print(ConnectingFrame.Text)
+			PlayAnimation(ID, ConnectingBox.Text)
+		else
+			Button.BackgroundColor3 = Red
+			PlayingId = 0
+		end
+	end)
+	Button.MouseButton2Click:Connect(function()
+		RenameBox.Text = ""
+		RenameBox.Visible = true
+		RenameBox:CaptureFocus()
+		local function Rename()
+			Button.Text = RenameBox.Text
+			RenameBox.Visible = false
+			Rename:Disconnect()
+		end
+		Rename = RenameBox.FocusLost:Connect(Rename)
+	end)
+	if Button.Parent == StolenAnimationsFrame then
+		Button.MouseEnter:Connect(function()
+			HoveringButton = Button
+			HoveringBox = ConnectingBox
+		end)
+		Button.MouseLeave:Connect(function()
+			HoveringButton = nil
+			HoveringBox = ConnectingBox
+		end)
+	end
+end
+
+function TextboxSpeedAdjust(Box,ID)
+	Box.FocusLost:Connect(function()
+		if not (PlayingId == ID or PlayingId == 0 or PlayingId == StealId) and not CursedMode then return end
+		if tonumber(Box.Text) == nil then
+			Track:AdjustSpeed(1)
+		else
+			--print("Yes")
+			Track:AdjustSpeed(tonumber(Box.Text))
+		end
+	end)
+end
+
+function CreateTextbox(Size,Color,X,Name,ID, Frame)
+	local Box = CreateInteractable(Size, Name, "Speed 1-5", Maroon, 0, false, "TextBox", Frame)
+	TextBoxRn = Box
+	TextboxSpeedAdjust(Box,"rbxassetid://"..ID)
+end
+
+function CreateTab(Size, Name, Text, Frame)
+	local Tab = CreateInteractable(Size, Name, Text, Red, 0, true, "TextButton", TabsFrame)
+	if Text == "Animations" then
+		Tab.BackgroundColor3 = Maroon
+	end
+	Tab.MouseButton1Click:Connect(function()
+		for Num, Tab in TabsFrame:GetChildren() do
+			if Tab:IsA("TextButton") then
+				Tab.BackgroundColor3 = Red
+			end
+		end
+		Tab.BackgroundColor3 = Maroon
+		for Num, Frame in ScreenGui:GetChildren() do
+			if Frame:IsA("ScrollingFrame") then
+				Frame.Visible = false
+			end
+		end
+		Frame.Visible = true
+	end)
+end
+
+function PlayAnimation(ID,Speed)
+	local Character = game.Players.LocalPlayer.Character
+
+	Anim.AnimationId = ID
+	--print(ID)
+
+	Track = Character:WaitForChild("Humanoid"):LoadAnimation(Anim)
+	Track:Play()
+	Track.Looped = true
+	--print(Track.Length)
+	
+	if tonumber(Speed) == nil then
+		Track:AdjustSpeed(1)
+	else
+		Track:AdjustSpeed(Speed)
+	end
+	Track.Priority = Enum.AnimationPriority.Action
+end
+
+function CreateSearchbar(Frame)
+	-- Search bar
+	local Searchbar = CreateInteractable(UDim2.new(0.95,0,0,45), "Searchbar", "Cari Emote", Red, 0, false, "TextBox", Frame)
+	Frame.CanvasSize += UDim2.new(0,0,0,Searchbar.Size.Y.Offset)
+	
+
+	-- Search bar functionality
+	Searchbar.Changed:Connect(function()
+		local Search = string.lower(Searchbar.Text)
+		for _, Button in Frame:GetChildren() do
+			if (Button:IsA("TextButton") or Button:IsA("TextBox")) and Button.Name ~= "SearchbarBox" then
+				if string.find(string.lower(Button.Name), Search) then
+					Button.Visible = true
+				else
+					Button.Visible = false
+				end
+			end
+		end
+	end)
+end
+
+function CFOAMeat(ID)
+	if Track then
+		Track:Stop()
+	end
+	PlayAnimation(ID,1)
+	MassUnToggle(nil)
+	StealStage = 0
+	PlayingId = "Steal"
+	StealId = ID
+	StealButton.BackgroundColor3 = Red
+end
+
+function CheckForOwnedAnimations(ID, PlayIt)
+	for Num, Frame in ScreenGui:GetChildren() do
+		if Frame:IsA("ScrollingFrame") then
+			for Num, Button in StolenAnimationsFrame:GetChildren() do
+				if Button:GetAttribute("AnimId") == ID then
+					if PlayIt then
+						CFOAMeat(ID)
+						Button:SetAttribute("Toggled", true)
+						Button.BackgroundColor3 = Maroon
+						--print("Play owned")
+						return true
+					else
+						return true
+					end
+				end
+			end
+		end
+	end
+	if PlayIt then
+		CFOAMeat(ID)
+		--print("Play unowned")
+	end
+	return false
+end
+
+-- Give layouts
+Layout(AnimationsFrame)
+Layout(StolenAnimationsFrame)
+Layout(TabsFrame)
+
+-- Create tabs
+NumOfTabs = 2
+CreateTab(UDim2.new(0,30,0,300/NumOfTabs), "AnimationsTab", "EMOTE", AnimationsFrame)
+CreateTab(UDim2.new(0,30,0,300/NumOfTabs), "StolenAnimationsTab", "RUSUH", StolenAnimationsFrame)
+
+-- Give search bars
+CreateSearchbar(AnimationsFrame)
+CreateSearchbar(StolenAnimationsFrame)
+
+-- Drag bar functionality
+DragBar.MouseButton1Down:Connect(function()
+	local Offset = game.Players.LocalPlayer:GetMouse().X - AnimationsFrame.AbsolutePosition.X
+	while game:GetService("UserInputService"):IsMouseButtonPressed(Enum.UserInputType.MouseButton1) do
+		task.wait(0.01)
+		AnimationsFrame.Position = UDim2.new(0,game.Players.LocalPlayer:GetMouse().X - Offset,0,game.Players.LocalPlayer:GetMouse().Y)
+		StolenAnimationsFrame.Position = AnimationsFrame.Position
+		Title.Position = UDim2.new(0,AnimationsFrame.AbsolutePosition.X,0, AnimationsFrame.AbsolutePosition.Y - Title.Size.Y.Offset*1.5)
+		TabsFrame.Position = AnimationsFrame.Position + UDim2.new(0, 213,0, 0)
+		DragBar.Position = UDim2.new(0,AnimationsFrame.AbsolutePosition.X-7.5,0, AnimationsFrame.AbsolutePosition.Y-HeaderSize)
+		CloseButton.Position = UDim2.new(0,AnimationsFrame.AbsolutePosition.X-7.5,0, AnimationsFrame.AbsolutePosition.Y-HeaderSize)
+	end
+end)
+
+--Close button functionality
+CloseButton.MouseButton1Click:Connect(function()
+	ScreenGui:Destroy()
+	script:Destroy()
+end)
+
+-- Steal button
+StealId = 0
+StealButton = CreateInteractable(UDim2.new(0.95,0,0,45), "StealButton", "Steal animation", Red, 100, true, "TextButton", StolenAnimationsFrame)
+StolenAnimationsFrame.CanvasSize += UDim2.new(0,0,0,StealButton.Size.Y.Offset)
+
+-- Steal button functionality
+StealStage = 0
+StealButton.MouseButton1Click:Connect(function()
+	--if not (PlayingId == StealId or PlayingId == 0) and not CursedMode then return end
+	if StealStage == 0 then
+		StealStage = 1
+		StealButton.BackgroundColor3 = Blue
+		local Highlight = Instance.new("Highlight",workspace.Terrain)
+		Highlight.FillColor = Blue
+		while StealStage == 1 do
+			task.wait(0.01)
+			--print(StealStage)
+			if Mouse.Target and Mouse.Target:FindFirstAncestorWhichIsA("Model"):FindFirstChildOfClass("Humanoid") then
+				Highlight.Adornee = Mouse.Target:FindFirstAncestorWhichIsA("Model")
+			else
+				Highlight.Adornee = nil
+			end
+		end
+		Highlight:Destroy()
+	elseif StealStage == 1 then
+		StealStage = 0
+		StealButton.BackgroundColor3 = Red
+	end
+	--print(StealStage)
+end)
+
+-- Emergency stop button
+EStopButton = CreateInteractable(UDim2.new(0.95,0,0,45), "EStopButton", "Stop Emote", Maroon, 0, true, "TextButton", AnimationsFrame)
+AnimationsFrame.CanvasSize += UDim2.new(0,0,0,StealButton.Size.Y.Offset)
+
+-- Emergency stop button functionality
+EStopButton.MouseButton1Click:Connect(function()
+	for Num, LocalTrack in Player.Character:WaitForChild("Humanoid"):GetPlayingAnimationTracks() do
+		LocalTrack:Stop()
+	end
+end)
+
+game:GetService("UserInputService").InputBegan:Connect(function(Input)
+	if (Input.UserInputType == Enum.UserInputType.MouseButton1 or Input.UserInputType == Enum.UserInputType.Touch) and StealStage == 1 then
+		local Target = Mouse.Target
+		if Target and Mouse.Target:FindFirstAncestorWhichIsA("Model"):FindFirstChildOfClass("Humanoid") then
+			local Humanoid = Mouse.Target:FindFirstAncestorWhichIsA("Model"):FindFirstChildOfClass("Humanoid")
+			--print(#(Humanoid:GetPlayingAnimationTracks()))
+			for Num, TheirTrack in Humanoid:GetPlayingAnimationTracks() do
+				local ID = TheirTrack.Animation.AnimationId
+				--print(Num, TheirTrack.Length ~= 0 and not CheckForOwnedAnimations(ID, true))
+				if TheirTrack.Length ~= 0 and not CheckForOwnedAnimations(ID, true) then
+					print(ID)
+					StealStage = 0
+					PlayingId = "Steal"
+					StealId = ID
+					StealButton.BackgroundColor3 = Red
+					--local Name = game:GetService("MarketplaceService"):GetProductInfo(tonumber(Track.Animation.AnimationId:sub(14))).Name
+					local animId = ID:match("%d+")
+					local Name = "Animation"
+					if animId then
+						local info = game:GetService("MarketplaceService"):GetProductInfo(tonumber(animId))
+						Name = info.Name
+					else
+						warn("No valid animation ID found.")
+					end
+					CreateTextbox(UDim2.new(0.45,0,0,45), DarkGrey, 0, Name, ID, StolenAnimationsFrame)
+					CreateButton(UDim2.new(0.45,0,0,45),Maroon, Name, Name, ID, StolenAnimationsFrame, TextBoxRn, true)
+					break
+				end
+			end
+		end
+	end
+	if Input.UserInputType == Enum.UserInputType.MouseButton3 and HoveringButton ~= nil then
+		if StealId == HoveringButton:GetAttribute("AnimId") and Track ~= nil then
+			Track:Stop()
+		end
+		print(HoveringButton.Size.Y.Offset)
+		StolenAnimationsFrame.CanvasSize -= UDim2.new(0,0,0,HoveringButton.Size.Y.Offset)
+		HoveringBox:Destroy()
+		HoveringButton:Destroy()
+	end
+end)
+
+-- Creating buttons
+
+Counter = 0
+
+for i=1, #AnimButtons/2 do
+	CreateTextbox(UDim2.new(0.45,0,0,45), Grey, 0, AnimButtons[Counter+2], AnimButtons[Counter+1], AnimationsFrame)
+	CreateButton(UDim2.new(0.45,0,0,45),Red, AnimButtons[Counter+2], AnimButtons[Counter+2], "rbxassetid://".. AnimButtons[Counter+1], AnimationsFrame, TextBoxRn, false)
+	Counter += 2
+end
